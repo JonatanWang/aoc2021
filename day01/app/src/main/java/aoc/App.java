97b82393449cb9aebd4627ae4bdc9500a948f904
JonatanWang <jonatan.wang@gmail.com>
2021-12-01 12:35:17 +0100

    Solution day 01

--- day01/app/src/main/java/aoc/App.java
+++ day01/app/src/main/java/aoc/App.java
@@ -3,16 +3,54 @@
  */
 package aoc;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.nio.file.Files.*;
+import static java.util.stream.Collectors.*;
+
 public class App {
+
+    private static final String FILE_PATH = "/Users/zhengyu.wang/dev/aoc2021/day01/app/src/main/java/aoc/input.txt";
+    private static List<Integer> input;
+
+    static {
+        try {
+            input = lines(Paths.get(FILE_PATH))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.<Integer>toList());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     public String getSolutionPart1() {
-        return "1337";
+        int cnt = 0;
+        for(int i = 0; i < input.size() - 1; i ++) {
+            if (input.get(i + 1) > input.get(i)) {
+                cnt ++;
+            }
+        }
+        return String.valueOf(cnt);
     }
 
     public String getSolutionPart2() {
-        return "42";
+        var cnt = 0;
+        for (int i = 0; i < input.size() - 3; i ++) {
+            var sumOfWindow1 = input.get(i) + input.get(i + 1) + input.get(i + 2);
+            var sumOfWindow2 = input.get(i + 1) + input.get(i + 2) + input.get(i + 3);
+            if (sumOfWindow2 > sumOfWindow1) cnt ++;
+        }
+        return String.valueOf(cnt);
     }
 
     public static void main(String[] args) {
+
         String part = System.getenv("part") == null ? "part1" : System.getenv("part");
         if (part.equals("part2"))
             System.out.println(new App().getSolutionPart2());
